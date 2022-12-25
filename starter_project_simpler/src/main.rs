f00d9bd4c80c658e70b1bbe4a5de023b32020b0f
Dotan Nahum <jondotan@gmail.com>
2022-12-25 19:38:58 +0200

    simple CLI should use clap_derive since there are no subcommands

--- starter_project_simpler/src/main.rs
+++ starter_project_simpler/src/main.rs
@@ -1,53 +1,48 @@
 #![allow(clippy::must_use_candidate)]
 
+use regex::Regex;
 use starter_project_simpler::data::CMD;
 use starter_project_simpler::runner;
 
 use anyhow::Result as AnyResult;
-use clap::{crate_version, ArgAction};
-use clap::{Arg, ArgMatches, Command};
-use console::style;
+use clap::{ArgAction, Parser};
 use std::process::exit;
 use tracing::metadata::LevelFilter;
 use tracing_subscriber::prelude::__tracing_subscriber_SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
 use tracing_subscriber::{EnvFilter, Registry};
 
-pub const BANNER: &str = r#"
-    B A N N E R
-"#;
+/// Zip eXtract: Unpack any archive
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Opts {
+    /// any kind of archive
+    #[arg(value_name = "ARCHIVE")]
+    archive: String,
 
-pub fn command() -> Command {
-    Command::new("starter_project")
-        .version(crate_version!())
-        .about("A starter project for Rust")
-        .arg(
-            Arg::new("dry_run")
-                .short('d')
-                .long("dry-run")
-                .value_name("EXAMPLE_KEY")
-                .help("Dry run with examples given in EXAMPLE_KEY"),
-        )
-        .arg(
-            Arg::new("reporter")
-                .short('r')
-                .long("reporter")
-                .value_name("REPORTER")
-                .value_parser(["console"])
-                .help("Reporter to use (default: 'console')"),
-        )
-        .arg(
-            Arg::new("no_banner")
-                .long("no-banner")
-                .help("Don't show the banner")
-                .action(ArgAction::SetTrue),
-        )
-        .arg(
-            Arg::new("verbose")
-                .long("verbose")
-                .help("Show details about interactions")
-                .action(ArgAction::SetTrue),
-        )
+    /// where to output extracted content
+    #[arg(value_name = "OUT_DIR")]
+    out_dir: String,
+
+    /// strip N number of components from the path of extracted files
+    #[arg(short, long, value_name = "NUM", default_value = "0")]
+    strip: Option<usize>,
+
+    /// only matching files will be extracted
+    #[arg(short, long, value_name = "REGEX")]
+    filter: Option<Regex>,
+
+    /// list files in archive (don't extract)
+    #[arg(short, long, action=ArgAction::SetTrue)]
+    list: bool,
+
+    /// list with metadata (don't extract)
+    #[arg(long, action=ArgAction::SetTrue)]
+    ls: bool,
+
+    /// list with metadata (don't extract)
+    #[arg(long, action=ArgAction::SetTrue)]
+    verbose: bool,
 }
 
 /// Run
@@ -56,30 +51,16 @@ pub fn command() -> Command {
 ///
 /// This function will return an error
 #[allow(clippy::unnecessary_wraps)]
-fn run(matches: &ArgMatches) -> AnyResult<bool> {
-    log::info!("default cmd {:?}", matches.get_one::<String>("reporter"));
+fn run(opts: &Opts) -> AnyResult<bool> {
+    log::info!("strip: {:?}", opts.strip);
     println!("going to run {CMD}");
     runner::run();
     Ok(true)
 }
 
 fn main() {
-    // just use $ LOG=1 mybin
-
-    let app = command();
-
-    let v = app.render_version();
-    let matches = app.get_matches();
-
-    if !matches.get_flag("no_banner") {
-        println!(
-            "{}\n                    {}",
-            style(BANNER).magenta(),
-            style(v).dim()
-        );
-    }
-
-    let level = if matches.get_flag("verbose") {
+    let opts = Opts::parse();
+    let level = if opts.verbose {
         LevelFilter::INFO
     } else {
         LevelFilter::OFF
@@ -98,14 +79,7 @@ fn main() {
         )
         .init();
 
-    // actual logic is in 'run'.
-    // subcommand is an error, but you can swap it later if you bring in subcommands
-    let res = match matches.subcommand() {
-        None => run(&matches),
-        _ => Ok(false),
-    };
-
-    match res {
+    match run(&opts) {
         Ok(ok) => {
             exit(i32::from(!ok));
         }
